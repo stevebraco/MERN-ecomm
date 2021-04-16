2f68de20bee4f0b46f9ea7fc72eea0e3cb9143f8
steve <steve.braco@gmail.com>
2021-04-16 13:21:46 +0200

    heroku config

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost:27017/amazona', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/amazona', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
